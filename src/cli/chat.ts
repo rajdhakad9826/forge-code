f1d815beb6eed6764cf49d193e5d29b82543a371
Raj Dhakad <officialrajdhakad@gmail.com>
2026-08-01 17:46:58 +0530

    feat: implement conversation history

--- src/cli/chat.ts
+++ src/cli/chat.ts
@@ -1,8 +1,16 @@
+import OpenAI from "openai";
 import { stdin, stdout } from "node:process";
 import { createInterface } from "node:readline/promises";
 import { chat } from "../llm/chat.js";
 
+const SYSTEM_PROMPT = "You are a helpful assistant.";
+
+
 export async function startChat(initialPrompt: string) {
+
+    const conversation: OpenAI.Responses.ResponseInput = [
+        { role: "system" as const, content: SYSTEM_PROMPT },
+    ]
 
     const rl = createInterface({
         input: stdin,
@@ -18,11 +26,19 @@ export async function startChat(initialPrompt: string) {
     console.log("Welcome to Forge Code!")
     console.log("--------------------------\n\n")
 
-    if (initialPrompt)
-        await chat(initialPrompt);
+    if (initialPrompt) {
+        conversation.push({ "role": "user", content: initialPrompt })
+        const assistantMessage = await chat(conversation);
+        if (typeof assistantMessage === "string")
+            conversation.push({ role: "assistant", content: assistantMessage })
+
+    }
 
     while (true) {
         const prompt = await rl.question("❯ ")
-        await chat(prompt)
+        conversation.push({ "role": "user", content: prompt })
+        const assistantMessage = await chat(conversation);
+        if (typeof assistantMessage === "string")
+            conversation.push({ role: "assistant", content: assistantMessage })
     }
 }
--- src/llm/chat.ts
+++ src/llm/chat.ts
@@ -1,27 +1,21 @@
 import OpenAI from "openai";
 import { client } from "./client.js";
-const SYSTEM_PROMPT = "You are a helpful assistant.";
 const MODEL = "openai/gpt-oss-120b";
 
-export async function chat(prompt: string) {
-
-    const context: OpenAI.Responses.ResponseInput = [
-        { role: "system" as const, content: SYSTEM_PROMPT },
-        { role: "user" as const, content: prompt }
-    ]
-
+export async function chat(conversation: OpenAI.Responses.ResponseInput) {
     process.stdout.write("Thinking...\n");
-
     const stream = await client.responses.create({
         model: MODEL,
-        input: context,
+        input: conversation,
         stream: true
     })
 
     for await (const event of stream) {
         if (event.type === "response.output_text.delta")
             process.stdout.write(event.delta)
-        else if (event.type === "response.completed")
+        else if (event.type === "response.output_text.done") {
             process.stdout.write('\n')
+            return event.text
+        }
     }
 }
