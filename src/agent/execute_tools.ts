750c697cbe6e5acbbc8a99b72a34c881eb147e36
Raj Dhakad <officialrajdhakad@gmail.com>
2026-08-19 15:30:19 +0530

    feat: implement error handling in execute_tools

--- src/agent/execute_tools.ts
+++ src/agent/execute_tools.ts
@@ -5,26 +5,37 @@ import type { ConversationItem, FunctionToolCall } from "../llm/types.js";
 export async function execute_tools(toolCalls: FunctionToolCall[], onPermissionRequest: (toolName: string, args: any) => Promise<boolean>) {
     let toolOutputs: ConversationItem[] = [];
     for (let tool of toolCalls) {
-        const args = JSON.parse(tool.arguments);
-        const toolName = tool.name;
+        try {
+            const args = JSON.parse(tool.arguments);
+            const toolName = tool.name;
 
-        const hasPermission = await askForPermission(toolName, args, onPermissionRequest)
+            const hasPermission = await askForPermission(toolName, args, onPermissionRequest)
 
-        if (!hasPermission) {
+            if (!hasPermission) {
+                toolOutputs.push({
+                    type: "function_call_output",
+                    call_id: tool.call_id,
+                    output: "User denied permission to execute this tool. Tell the user what you need to do differently."
+                });
+                continue;
+            }
+            if (!toolRegistry[toolName])
+                throw new Error(`Tool "${toolName}" does not exist.`);
+
+            const toolResult = await toolRegistry[toolName].callback(args);
             toolOutputs.push({
                 type: "function_call_output",
                 call_id: tool.call_id,
-                output: "User denied permission to execute this tool. Tell the user what you need to do differently."
+                output: toolResult
             });
-            continue;
+        } catch (error) {
+            const message = error instanceof Error ? error.message : String(error);
+            toolOutputs.push({
+                type: "function_call_output",
+                call_id: tool.call_id,
+                output: message
+            })
         }
-
-        const toolResult = await toolRegistry[toolName].callback(args);
-        toolOutputs.push({
-            type: "function_call_output",
-            call_id: tool.call_id,
-            output: toolResult
-        });
     }
     return toolOutputs
 }
